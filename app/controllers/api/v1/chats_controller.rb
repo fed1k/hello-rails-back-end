356e73971d01766c94b1dc88ccf60dfc2005c2c0
fed1k <allamurotovcoder@mail.ru>
2022-08-26 16:58:42 -0400

    Setup Api which returns random greeting

---
+++ app/controllers/api/v1/chats_controller.rb
@@ -0,0 +1,51 @@
+class Api::V1::ChatsController < ApplicationController
+  before_action :set_chat, only: %i[ show update destroy ]
+
+  # GET /chats
+  def index
+    @chats = Chat.all
+
+    render json: @chats
+  end
+
+  # GET /chats/1
+  def show
+    render json: @chat
+  end
+
+  # POST /chats
+  def create
+    @chat = Chat.new(chat_params)
+
+    if @chat.save
+      render json: @chat, status: :created, location: @chat
+    else
+      render json: @chat.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /chats/1
+  def update
+    if @chat.update(chat_params)
+      render json: @chat
+    else
+      render json: @chat.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /chats/1
+  def destroy
+    @chat.destroy
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_chat
+      @chat = Chat.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def chat_params
+      params.require(:chat).permit(:greeting)
+    end
+end
